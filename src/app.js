ba536b372c044f7764d56a96a9cb252c3df61778
JessyQT <60488881+JessyQuinto@users.noreply.github.com>
2024-12-20 09:21:40 -0500

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//C:\Users\Jessy\source\repos\accessibility-backend\src\app.js
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+//C:\Users\Jessy\source\repos\accessibility-backend\src\server.js
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
